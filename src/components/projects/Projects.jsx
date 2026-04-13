2731769eda80b7d81a830276311196afa51df3cb
Mohamed Ahmed <m7mda7mdricherd75@gmail.com>
2026-04-13 13:36:28 +0200

    Adding new project (Crypto Money Tracker) with new statments improved if no demo link shows "no demo yet"

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,6 +5,7 @@ import { faCode } from "@fortawesome/free-solid-svg-icons";
 export default function Projects() {
   const projectList = [
     {
+      id: 1,
       title: "ToDo List",
       description:
         "A simple and intuitive task management application that allows users to add, edit, delete, and organize daily tasks efficiently. It features local storage to ensure data persistence and a clean, user-friendly interface.",
@@ -13,6 +14,7 @@ export default function Projects() {
       repoLink: "https://github.com/M7madMeDo/TodoList.git",
     },
     {
+      id: 2,
       title: "Bookmarker",
       description:
         "A web application designed to save and manage favorite website links. Users can easily add, visit, and delete bookmarks, with data stored locally for quick and reliable access.",
@@ -21,6 +23,7 @@ export default function Projects() {
       repoLink: "https://github.com/M7madMeDo/bookmarker.git",
     },
     {
+      id: 3,
       title: "React Portfolio",
       description:
         "A modern and responsive personal portfolio built with React. It showcases projects, skills, and contact information through reusable components and a clean, professional design.",
@@ -28,16 +31,31 @@ export default function Projects() {
       liveLink: "m7mad-portfolio.netlify.app",
       repoLink: "https://github.com/M7madMeDo/myPortfolio.git",
     },
+    {
+      id: 4,
+      title: "Crypto Money Tracker",
+      description:
+        "A responsive React application that displays cryptocurrency market data using the CoinGecko API. The app features a clean UI, dynamic data fetching, and smooth pagination for an enhanced user experience.",
+      image: "/assets/pics/Cypto projeect.png",
+      liveLink: "",
+      repoLink: "https://github.com/M7madMeDo/crypto-money-tracker",
+    },
   ];
+
   return (
     <section id="projects" className="py-5 bg-light">
       <Container>
         <h2 className="text-center fw-bold mb-5 text-dark">
           Featured Projects
         </h2>
-        <Row className="g-4 align-items-stretch justify-content-center">
-          {projectList.map((project, index) => (
-            <Col md={6} lg={4} key={index} className="d-flex">
+        <Row className="g-4   justify-content-start">
+          {projectList.map((project) => (
+            <Col
+              md={6}
+              lg={4}
+              key={project.id}
+              className="d-flex align-items-center "
+            >
               <Card className="w-100 shadow-sm border-0 d-flex flex-column">
                 <Card.Img
                   variant="top"
@@ -55,12 +73,12 @@ export default function Projects() {
                   <div className="d-flex justify-content-between mt-auto">
                     <Button
                       variant="primary"
-                      href={project.liveLink}
+                      href={project.liveLink || "#"}
                       target="_blank"
                       className="d-flex align-items-center"
                     >
-                      <FontAwesomeIcon icon={faCode} className="me-2" /> Live
-                      Demo
+                      <FontAwesomeIcon icon={faCode} className="me-2" />
+                      {project.liveLink ? "Live Demo" : "No Demo yet"}
                     </Button>
                     <Button
                       variant="outline-dark"
